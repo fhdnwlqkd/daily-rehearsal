7538f2ca369156935891863552c121bbbff14b16
mincheol <fhdnwlqkd@hanyang.ac.kr>
2026-05-02 16:08:53 +0900

    [#3] spring initialization

---
+++ backend/mirror-api/src/test/java/com/stc/mirror_api/MirrorApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.stc.mirror_api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MirrorApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
