a32c64f66751b084e6416b69f474a40fecccc3f2
mincheol <fhdnwlqkd@hanyang.ac.kr>
2026-05-05 17:16:59 +0900

    [#3] 인프라 설정

--- backend/mirror-api/src/test/java/com/stc/mirror_api/MirrorApiApplicationTests.java
+++ backend/mirror-api/src/test/java/com/stc/mirror_api/MirrorApiApplicationTests.java
@@ -2,7 +2,9 @@ package com.stc.mirror_api;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
 
+@ActiveProfiles("test")
 @SpringBootTest
 class MirrorApiApplicationTests {
 
